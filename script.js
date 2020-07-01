922baf6f86f8696a5830c5ab4fbabf1f5f88563d
keisuke-tnk01 <ashitama18@gmail.com>
2020-07-01 22:39:44 +0900

    add jQuery to sidebar menu
    sidebar menuのon/off表示のjQueryを実装

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-$("js-open_menu").on("click", function() {
-  $(this).next().slideToggle();
-  $(this).toggleClass("active");
+/*$('#js-open_menu').click(function() {
+  $('sidebar').css('right', '0'),
+  $('sidebar').css('position', 'absolute');
+  $('header', 'main', 'footer').css('position', 'fixed');
+  $('header', 'main', 'footer').css('opacity', '0.2');
 });
+
+$('#js-close_menu').click(function() {
+  $('sidebar').css('right', '-35rem');
+  $('sidebar').css('position', 'fixed');
+  $('header', 'main', 'footer').css('position', 'absolute');
+  
+});*/
+
+
+$(function() {
+  $('#js-open_menu').click(function() {
+    $('#js-sidebar_wrapper').fadeIn();
+    $('.l-open_menu-wrapper').fadeIn();
+    $('header', 'main', 'footer').css('position', 'fixed');
+  });
+});
+
+  
+ 
+$(function() {  
+  // 「.close-modal」要素にclickイベントを設定してください
+  $('#js-close_menu').click(function(){
+    $('#js-sidebar_wrapper').fadeOut();
+    $('.l-open_menu-wrapper').fadeOut();
+    $('header', 'main', 'footer').css('position', 'absolute');
+  });
+  
+});
+
