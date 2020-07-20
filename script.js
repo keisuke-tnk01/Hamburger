ad10aecf4838dadee8f5c39d3fca3752ddead670
keisuke-tnk01 <ashitama18@gmail.com>
2020-07-20 10:46:48 +0900

    add scroll function to sidebar
    JSでsidebar表示時に背景固定でsidebarのみスクロールさせる機能を実装

--- script.js
+++ script.js
@@ -1,36 +1,19 @@
-
-
-/*$(function () {
-  $('#js-open_menu').click(function () {
-    $('.l-open_menu-wrapper').fadeIn();
-    $('#js-sidebar_wrapper').animate({ "marginRight": "35rem" });
-    $('.l-sidebar_wrapper').css('overflow', 'auto');
-  });
-});*/
-
-/*let menuScroll = function() {
-  $('sidebar').css('position', 'fixed');
-}
-let time =4000;
-setTimeout(menuScroll, time);*/
-
-
 $(function () {
   $('#js-open_menu').click(function () {
     $('.l-open_menu-wrapper').fadeIn();
-    $('#js-sidebar_wrapper').addClass('is-open');
+    $('.l-sidebar_wrapper').addClass('is-open');
     $('.l-menu_cancel_btn').addClass('is-open');
     $('.l-sidebar_title').addClass('is-open');
-    $(body).addClass('is-open');
+    $('body').addClass('is-open');
   });
 });
 
 $(function () {
   $('#js-close_menu').click(function () {
-    $('#js-sidebar_wrapper').removeClass('is-open');
+    $('.l-sidebar_wrapper').removeClass('is-open');
     $('.l-menu_cancel_btn').removeClass('is-open');
     $('.l-sidebar_title').removeClass('is-open');
     $('.l-open_menu-wrapper').fadeOut();
-    $(body).removeClass('is-open');
+    $('body').removeClass('is-open');
   });
 });
