4b6ea37f019cdbc66783bd630ab54f1da72bb145
keisuke-tnk01 <ashitama18@gmail.com>
2020-07-25 00:53:38 +0900

    fix class name for FLOCSS
    クラス名称をFLOCSSのために再定義

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 $(function () {
-  $('#js-open_menu').click(function () {
-    $('.c-open_menu-wrapper').fadeIn();
-    $('.p-sidebar_wrapper').addClass('is-open');
-    $('.c-menu_cancel_btn').addClass('is-open');
-    $('.c-sidebar_title').addClass('is-open');
+  $('#js-open__menu').click(function () {
+    $('.u-open__menu__wrapper').fadeIn();
+    $('.p-sidebar__wrapper').addClass('is-open');
+    $('.c-menu__cancel__btn').addClass('is-open');
+    $('.c-sidebar__title').addClass('is-open');
     $('body').addClass('is-open');
   });
 });
 
 $(function () {
-  $('#js-close_menu').click(function () {
-    $('.p-sidebar_wrapper').removeClass('is-open');
-    $('.c-menu_cancel_btn').removeClass('is-open');
-    $('.c-sidebar_title').removeClass('is-open');
-    $('.c-open_menu-wrapper').fadeOut();
+  $('#js-close__menu').click(function () {
+    $('.p-sidebar__wrapper').removeClass('is-open');
+    $('.c-menu__cancel__btn').removeClass('is-open');
+    $('.c-sidebar__title').removeClass('is-open');
+    $('.u-open__menu__wrapper').fadeOut();
     $('body').removeClass('is-open');
   });
 });
